dffd44d0574afbf26f0b3d86b3cb7680cb87903f
KimInteger <kjs20151240@gmail.com>
2024-05-07 14:51:23 +0900

    오전수업

---
+++ firstProject/projectFocus.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+
+const leftBtn = document.getElementById('leftMove');
+
+const rightBtn = document.getElementById('rightMove');
+
+const partition = document.getElementById('partition');
