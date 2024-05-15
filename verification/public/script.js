b4d27db690d4d8f356a18b22d7861d578a4214e4
KimInteger <kjs20151240@gmail.com>
2024-05-15 18:23:36 +0900

    버튼클릭화면이동구현

--- verification/public/script.js
+++ verification/public/script.js
@@ -1,15 +1,14 @@
 const root = document.getElementById('root');
 
+const show = document.getElementById('show');
+
 const leftmove = document.getElementById('leftmove');
 LMS = leftmove.style;
 LMS.position = "absolute";
 LMS.top = "50%";
-LMS.left = "45vw";
+LMS.left = "55%";
 LMS.fontSize = "3rem";
 LMS.backgroundColor = "transparent";
-LMS.flexShrink = 0;
-LMS.width = "10vw";
-LMS.heigth = "10vw";
 LMS.zIndex = "4";
 LMS.border = "none";
 LMS.cursor = "pointer";
@@ -17,19 +16,51 @@ LMS.cursor = "pointer";
 const rightmove = document.getElementById('rightmove');
 RMS = rightmove.style;
 RMS.position = "absolute";
-RMS.position = "absolute";
 RMS.top = "50%";
-RMS.left = "90vw";
+RMS.right = "5%";
 RMS.fontSize = "3rem";
 RMS.backgroundColor = "transparent";
-RMS.flexShrink = 0;
-RMS.width = "10vw";
-RMS.heigth = "10vw";
 RMS.zIndex = "4";
 RMS.border = "none";
 RMS.cursor = "pointer";
 
-const eg = document.getElementById('EG');
-const hg = document.getElementById('HG');
-const rg = document.getElementById('RG');
-const mg = document.getElementById('MG');
+let grade = ["eg","hg","rg","mg"];
+
+function MoveR(){
+  for(let i = 0; i < grade.length; i++){
+    if (show.children[i].style.transform === "translate(0px)") {
+      show.children[i].style.transform = "translate(-695px)";
+    } else if (show.children[i].style.transform === "translate(-695px)") {
+      show.children[i].style.transform = "translate(-1390px)";
+    } else if (show.children[i].style.transform === "translate(-1390px)") {
+      show.children[i].style.transform = "translate(-2085px)";
+      RMS.color = 'white';
+      LMS.color = 'white';
+    } else {
+      show.children[i].style.transform = "translate(0px)";
+      RMS.color = 'black';
+      LMS.color = 'black';
+    }
+  }
+};
+
+function MoveL(){
+  for(let i = 0; i < grade.length; i++){
+    if (show.children[i].style.transform === "translate(0px)") {
+      show.children[i].style.transform = "translate(-2085px)";
+      RMS.color = 'white';
+      LMS.color = 'white';
+    } else if (show.children[i].style.transform === "translate(-2085px)") {
+      show.children[i].style.transform = "translate(-1390px)";
+      RMS.color = 'black';
+      LMS.color = 'black';
+    } else if (show.children[i].style.transform === "translate(-1390px)") {
+      show.children[i].style.transform = "translate(-695px)";
+    } else {
+      show.children[i].style.transform = "translate(0px)";
+    }
+  }
+}
+
+rightmove.addEventListener('click', MoveR);
+leftmove.addEventListener('click', MoveL);
